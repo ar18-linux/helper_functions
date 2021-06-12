0737891f41bf53ca769773df006cdf5c7fdf6deb
nulysses <adr.zivil@gmail.com>
2021-06-12 12:01:16 +0200

    1

---
+++ helper_functions/helper_functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+function obtain_sudo_password() {
+  if [ -z "${ar18_sudo_passwd+x}" ]; then
+    echo "Testing for sudo capabilities..."
+    set +e
+    timeout 2 sudo id || (echo "User $(whoami) does not have sudo rights, aborting"; exit 1)
+    set -e
+    read -s -p "Enter your password: " sudo_passwd
+    export ar18_sudo_passwd="${sudo_passwd}"
+  fi
+}
+
+
+function pacman_install() {
+  local errexit="$(shopt -op | grep errexit)"
+  set -e
+  packages="$1"
+  obtain_sudo_password
+  if [ -z "${ar18_pacman_cache_updated+x}" ]; then
+    echo "${ar18_sudo_passwd}" | sudo -S -k pacman -Sy
+    export ar18_pacman_cache_updated=1
+  fi
+  echo "${ar18_sudo_passwd}" | sudo -S -k pacman -S "${packages}" --noconfirm
+  "${errexit}"
+}
