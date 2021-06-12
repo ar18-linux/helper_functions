411fffa558e8055acd01dfde3fa8deea17903cbe
nulysses <adr.zivil@gmail.com>
2021-06-12 19:03:27 +0200

    1

--- helper_functions/helper_functions.sh
+++ helper_functions/helper_functions.sh
@@ -89,8 +89,8 @@ function obtain_sudo_password() {
   if [ -z "${ar18_sudo_password+x}" ]; then
     echo "Testing for sudo capabilities..."
     
-    has_sudo_capabilities
-    if [ "$?" = "1" ]; then
+    #has_sudo_capabilities
+    if [ $(has_sudo_capabilities) ]; then
       echo "Sudo rights have been asserted"
     else
        read -p "[ERROR]: User $(whoami) does not have sudo rights, aborting"; 
