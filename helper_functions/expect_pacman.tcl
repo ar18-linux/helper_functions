e11e59bcf9fec903a539d5f64dba8ae4dbc3a64c
nulysses <adr.zivil@gmail.com>
2021-06-13 15:04:50 +0200

    1

--- helper_functions/expect_pacman.tcl
+++ helper_functions/expect_pacman.tcl
@@ -5,8 +5,11 @@ set packages [lindex $argv 0];
 set timeout -1
 
 spawn pacman -S "$packages" --noconfirm --needed
-expect "Enter a selection"
-send -- "\r"
-
-expect eof
+expect {
+  "Enter a selection" {
+    send -- "\r"
+  }
+  eof {
+    exit
+  }
 }
