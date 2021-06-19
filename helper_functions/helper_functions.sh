111427e40b808cc059327997aea9540b24fdc5b4
nulysses <adr.zivil@gmail.com>
2021-06-19 14:10:19 +0200

    1

--- helper_functions/helper_functions.sh
+++ helper_functions/helper_functions.sh
@@ -201,6 +201,50 @@ function pip_install() {
 export -f pip_install
 
 
+function read_target() {
+  # Function template 2021-06-12.01
+  local LD_PRELOAD_old
+  LD_PRELOAD_old="${LD_PRELOAD}"
+  LD_PRELOAD=
+  local shell_options
+  IFS=$'\n' shell_options=($(shopt -op))
+  set -eu
+  set -o pipefail
+  local ret
+  ret=0
+  set +x
+  ##############################FUNCTION_START#################################
+  
+  if [ ! -v ar18_deployment_target ]; then
+    local ar18_deployment_target2
+    set +u
+    ar18_deployment_target2="${1}"
+    set -u
+    if [ "${ar18_deployment_target2}" = "" ]; then
+      if [ ! -f "/home/$(logname)/.config/ar18/deploy/installed_target" ]; then
+        read -p "[ERROR]: Cannot find file to determine installed_target"
+        exit 1
+      else
+        ar18_deployment_target2="$(cat "/home/$(logname)/.config/ar18/deploy/installed_target")"
+        echo "${ar18_deployment_target2}"
+      fi
+    else
+      echo "${ar18_deployment_target2}"
+    fi
+  else 
+    echo "${ar18_deployment_target}"
+  fi
+  
+  ###############################FUNCTION_END##################################
+  set +x
+  for option in "${shell_options[@]}"; do
+    eval "${option}"
+  done
+  LD_PRELOAD="${LD_PRELOAD_old}"
+  return "${ret}"
+}
+
+
 function source_or_execute_config() {
   # Function template 2021-06-12.01
   local LD_PRELOAD_old
@@ -265,6 +309,7 @@ function source_or_execute_config() {
   LD_PRELOAD="${LD_PRELOAD_old}"
   return "${ret}"
 }
+export -f source_or_execute_config
 
 
 function aur_install() {
