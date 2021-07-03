8c2b84fafc1358bed7f1d5b7242d3da4f760cd8c
nulysses <adr.zivil@gmail.com>
2021-07-03 12:14:22 +0200

    1

--- helper_functions/version_checker.sh
+++ helper_functions/version_checker.sh
@@ -24,7 +24,8 @@ if [ -f "${ar18_version_checker_dir_name}/VERSION" ]; then
       mkdir -p "/tmp/${ar18_version_checker_module_name}"
       cd "/tmp/${ar18_version_checker_module_name}"
       git clone "http://github.com/ar18-linux/${ar18_version_checker_module_name}"
-      cp -rf "/tmp/${ar18_version_checker_module_name}/${ar18_version_checker_module_name}" "${ar18_version_checker_dir_name}"
+      cp -raf "/tmp/${ar18_version_checker_module_name}/${ar18_version_checker_module_name}/${ar18_version_checker_module_name}/." "${ar18_version_checker_dir_name}/"
+      ar18_version_checker_caller
     fi
   fi
   
