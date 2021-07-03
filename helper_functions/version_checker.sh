7aef96664dc72a67134718bbcda5d85692bfe4f9
nulysses <adr.zivil@gmail.com>
2021-07-03 09:03:42 +0200

    1

--- helper_functions/version_checker.sh
+++ helper_functions/version_checker.sh
@@ -8,9 +8,12 @@ if [ -f "${dirname}/VERSION" ]; then
   rm -f /tmp/VERSION
   wget "https://raw.githubusercontent.com/ar18-linux/${ar18_version_cheker_module_name}/master/${ar18_version_cheker_module_name}/VERSION" -P /tmp
   ar18_version_cheker_module_version_remote="$(cat "/tmp/VERSION")"
-  if [ -f "/home/$(whoami)/.config/${ar18_version_cheker_module_name}/INSTALL_DIR" ]; then
-    
+  if [ "${ar18_version_cheker_module_version_remote}" > "${ar18_version_cheker_module_version_local}" ]; then
+    echo "new version available"
   fi
+  #if [ -f "/home/$(whoami)/.config/${ar18_version_cheker_module_name}/INSTALL_DIR" ]; then
+    
+  #fi
 fi
 #rm -rf "/tmp/helper_functions_$(whoami)"; 
 #mkdir -p "/tmp/helper_functions_$(whoami)"; cd "/tmp/helper_functions_$(whoami)";
