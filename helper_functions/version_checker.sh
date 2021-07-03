fdbf6a9c13c8a7d092e47375f7ce5c2750c30658
nulysses <adr.zivil@gmail.com>
2021-07-03 09:08:12 +0200

    1

--- helper_functions/version_checker.sh
+++ helper_functions/version_checker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "checking version..."
 ar18_version_cheker_caller="$(caller | cut -d ' ' -f2-)"
 ar18_version_cheker_dir_name="$(dirname "${ar18_version_cheker_caller}")"
 ar18_version_cheker_module_name="$(basename "${ar18_version_cheker_dir_name}")"
