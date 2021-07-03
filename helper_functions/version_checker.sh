14d102723775885f18c9f6d7c814f1bf5cca7238
nulysses <adr.zivil@gmail.com>
2021-07-03 12:04:55 +0200

    1

--- helper_functions/version_checker.sh
+++ helper_functions/version_checker.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-echo "checking version..."
-ar18_version_cheker_caller="$(caller | cut -d ' ' -f2-)"
-ar18_version_cheker_dir_name="$(dirname "${ar18_version_cheker_caller}")"
-ar18_version_cheker_module_name="$(basename "${ar18_version_cheker_dir_name}")"
-if [ -f "${dirname}/VERSION" ]; then
-  ar18_version_cheker_module_version_local="$(cat "${dirname}/VERSION")"
+set -x
+
+echo "checking version...4"
+ar18_version_checker_caller="$(caller | cut -d ' ' -f2-)"
+ar18_version_checker_caller="$(realpath "${ar18_version_checker_caller}")"
+ar18_version_checker_dir_name="$(dirname "${ar18_version_checker_caller}")"
+ar18_version_checker_module_name="$(basename "${ar18_version_checker_dir_name}")"
+if [ -f "${ar18_version_checker_dir_name}/VERSION" ]; then
+  ar18_version_checker_module_version_local="$(cat "${ar18_version_checker_dir_name}/VERSION")"
   rm -f /tmp/VERSION
-  wget "https://raw.githubusercontent.com/ar18-linux/${ar18_version_cheker_module_name}/master/${ar18_version_cheker_module_name}/VERSION" -P /tmp
-  ar18_version_cheker_module_version_remote="$(cat "/tmp/VERSION")"
-  echo "local version is ${ar18_version_cheker_module_version_local}"
-  echo "remote version is ${ar18_version_cheker_module_version_remote}"
-  if [[ "${ar18_version_cheker_module_version_remote}" > "${ar18_version_cheker_module_version_local}" ]]; then
+  wget "https://raw.githubusercontent.com/ar18-linux/${ar18_version_checker_module_name}/master/${ar18_version_checker_module_name}/VERSION" -P /tmp
+  ar18_version_checker_module_version_remote="$(cat "/tmp/VERSION")"
+  echo "local version is ${ar18_version_checker_module_version_local}"
+  echo "remote version is ${ar18_version_checker_module_version_remote}"
+  if [[ "${ar18_version_checker_module_version_remote}" > "${ar18_version_checker_module_version_local}" ]]; then
     echo "new version available"
+    if [ -f "/home/$(whoami)/.config/${ar18_version_checker_module_name}/INSTALL_DIR" ]; then
+      echo "reinstalling"
+    else
+      echo "replacing"
+      rm -rf "/tmp/${ar18_version_checker_module_name}"
+      mkdir -p "/tmp/${ar18_version_checker_module_name}"
+      cd "/tmp/${ar18_version_checker_module_name}"
+      git clone "http://github.com/ar18-linux/${ar18_version_checker_module_name}"
+      cp -rf "/tmp/${ar18_version_checker_module_name}/${ar18_version_checker_module_name}" "${ar18_version_checker_dir_name}"
+    fi
   fi
-  #if [ -f "/home/$(whoami)/.config/${ar18_version_cheker_module_name}/INSTALL_DIR" ]; then
-    
-  #fi
+  
 fi
 #rm -rf "/tmp/helper_functions_$(whoami)"; 
 #mkdir -p "/tmp/helper_functions_$(whoami)"; cd "/tmp/helper_functions_$(whoami)";
